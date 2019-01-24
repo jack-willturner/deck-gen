91714642e64eba3776e600c12f5c6c9954b95108
Jack Turner <jackwilliamturner@icloud.com>
2019-01-24 16:13:15 +0000

    little bit of cleanup - moving away from translation because it's too painful

---
+++ README.md
@@ -0,0 +1,4 @@
+# deck-gen
+Generate Anki decks with translations of the most frequent words in the subtitles of a TV show.   
+
+Not sorry about how shoddy this code is.
--- generator.py
+++ generator.py
@@ -2,15 +2,14 @@ import glob
 import os
 import xml.etree.ElementTree as ET
 import argparse
-from googletrans import Translator
+import re
 from collections import Counter
-
-directory = '.'
-translator = Translator()
+from collections import Iterable
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input", type=str, default=directory)
-parser.add_argument("-o", "--output", type=str, default=directory)
+parser.add_argument("-i", "--input", type=str, default='./xml', help='Where are the subtitles relative to me?')
+parser.add_argument("-o", "--output", type=str, default='./decks', help='Where do you want me to save the decks I generate?')
 args = parser.parse_args()
 
 episodes = []
@@ -23,19 +22,28 @@ def get_lines_from_episode(xml_file):
     for child in ep_tree.iter():
         if child.text:
             words.append(child.text)
-    #ep_list = ET.fromstringlist(xml_file)
+
     return words
+
+# with thanks to stackoverflow
+def flatten(l):
+    for el in l:
+        if isinstance(el, Iterable) and not isinstance(el, (str, bytes)):
+            yield from flatten(el)
+        else:
+            yield el
+
+def filter_non_words(l):
+    p = re.compile('[-\[\]:?.]') # it turns out i have no idea how to use regex's any more
+    return list([w.lower() for w in l if p.search(w) is None])
 
 os.chdir(args.input)
 for file in glob.glob("*.xml"):
     lines = get_lines_from_episode(file)
-    words = [w for line in lines for w in line.split()]
+    words = filter_non_words(list(flatten([w for line in lines for w in line.split()])))
+    translations = []
 
     word_freqs = Counter(words)
-
-    translations = translator.translate(words)
-    print(translations)
     episodes.append(words)
 
-for k, w in translations.items():
-    print(k, w)
+print(translations)
