98871f42c13a26a6d89dbdbce2ab53588f88d540
Jack Turner <jackwilliamturner@icloud.com>
2019-01-26 00:34:40 +0000

    basic deck generation now works

--- generator.py
+++ generator.py
@@ -2,6 +2,7 @@ import glob
 import os
 import xml.etree.ElementTree as ET
 import argparse
+import genanki
 import re
 from collections import Counter
 from collections import Iterable
@@ -12,28 +13,92 @@ parser.add_argument("-i", "--input", type=str, default='./xml', help='Where are 
 parser.add_argument("-o", "--output", type=str, default='./decks', help='Where do you want me to save the decks I generate?')
 args = parser.parse_args()
 
+# anki defaults
+model_id = 1237575582
+deck_id  = 1250240356
+
+
+my_model = genanki.Model(
+  model_id,
+  'Simple Model',
+  fields=[
+    {'name': 'Question'},
+    {'name': 'Answer'},
+  ],
+  templates=[
+    {
+      'name': 'Card 1',
+      'qfmt': '{{Question}}',
+      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+    },
+  ])
+
+my_deck = genanki.Deck(
+    deck_id,
+    'Marseilles'
+)
+
 episodes = []
 num_episodes = 1
 
+class Row:
+    def __init__(self, begin, end, text):
+        self.begin = begin
+        self.end   = end
+        self.text  = text
+
+def transpose_xmltree(tree):
+    rows = [] # use a list since we'll be traversing anyway
+    for child in tree.iter():
+        if 'begin' in child.attrib:
+            begin = int(child.attrib['begin'][:-1])
+            end   = int(child.attrib['end'][:-1])
+            text  = child.text
+
+            newRow = Row(begin, end, text)
+            rows.append(newRow)
+    return rows
+
+def get_overlap(a, b):
+    return max(0, min(a[1], b[1]) - max(a[0], b[0]))
+
+# horrible range intersection code
+def get_best_overlap(s_child, target_tree):
+    s_range = range(s_child.begin, s_child.end)
+    size_of_range = len(s_range)
+    percentage_threshold = int(size_of_range * 0.8)
+
+    # we're going to traverse the target tree and find the most overlapping subtitle
+    best_overlap_node = None
+
+    for t_child in target_tree:
+        # get the time range for the node
+        overlap = get_overlap((s_child.begin, s_child.end), (t_child.begin, t_child.end))
+        if overlap > percentage_threshold:
+            best_overlap = overlap
+            best_overlap_node = t_child
+            break
+
+        if (t_child.begin > s_child.end):
+            best_overlap_node = None
+            break
+
+    return best_overlap_node
+
+
 def get_lines_from_episode(source, target):
-    source_tree, target_tree = ET.parse(source), ET.parse(target)
+    source_tree, target_tree = transpose_xmltree(ET.parse(source)), transpose_xmltree(ET.parse(target))
     s_lines, t_lines = [], []
     p = re.compile('[\[\]]')
-    for s_child, t_child in zip(source_tree.iter(), target_tree.iter()):
-        if s_child.text and t_child.text:
-            if (p.search(s_child.text) is None):
-                s_lines.append(s_child.text)
-                t_lines.append(t_child.text)
+
+    for s_child in source_tree:
+        t_child = get_best_overlap(s_child, target_tree)
+        if t_child is not None:
+            s_lines.append(s_child.text)
+            t_lines.append(t_child.text)
 
     return s_lines, t_lines
 
-# with thanks to stackoverflow
-def flatten(l):
-    for el in l:
-        if isinstance(el, Iterable) and not isinstance(el, (str, bytes)):
-            yield from flatten(el)
-        else:
-            yield el
 
 def filter_non_words(l):
     p = re.compile('[-\[\]:?.]') # it turns out i have no idea how to use regex's any more
@@ -46,12 +111,13 @@ for ep_num in range(1, num_episodes+1):
     source_lines, target_lines = get_lines_from_episode(source, target)
 
     for s,t in zip(source_lines, target_lines):
-        print(s, "\t\t\t : ", t)
+        if (s is not None) and (t is not None):
+            note = genanki.Note(
+                model=my_model,
+                fields=[s, t]
+            )
+            my_deck.add_note(note)
 
-
-
-    #words = filter_non_words(list(flatten([w for line in lines for w in line.split()])))
-    #translations = []
-
-    #word_freqs = Counter(words)
-    #episodes.append(words)
+os.chdir('../decks')
+pack = genanki.Package(my_deck)
+pack.write_to_file('marseilles_ep1.apkg')
